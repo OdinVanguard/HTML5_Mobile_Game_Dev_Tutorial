42db91d44cd106b72e0c2e03f284f99ddefd914c
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2017-08-07 11:12:08 -0700

    setup scene for chapter5; background added

--- chapter5/chapter5.js
+++ chapter5/chapter5.js
@@ -5,68 +5,31 @@ goog.provide('chapter5');
 //get requirements
 goog.require('lime.Director');
 goog.require('lime.Scene');
-goog.require('lime.Layer');
-goog.require('lime.Circle');
-goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
-goog.require('lime.animation.MoveTo');
+goog.require('lime.Sprite');
+goog.require('lime.fill.LinearGradient');
 
 
 // entrypoint
 chapter5.start = function(){
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
-
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
-
-
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
-
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
-        });
-
-
-    });
-
+    var director = new lime.Director(document.body,1024,768);
+    director.makeMobileWebAppCapable();
+    director.setDisplayFPS(false);
+    
+    var scene1 = new lime.Scene();
+    
+    var grass_gradient = new lime.fill.LinearGradient();
+    grass_gradient.setDirection(0,0,1,-1);
+    grass_gradient.addColorStop(0,"#7CCD7C");
+    grass_gradient.addColorStop(0.5,"#00FF00");
+    
+    var grass = new lime.Sprite();
+    grass.setSize(800,640).setPosition(0,0).setAnchorPoint(0,0).setFill(grass_gradient);
+    
+    scene1.appendChild(grass);
+    
 	// set current scene active
-	director.replaceScene(scene);
+	director.replaceScene(scene1);
 
 }
 

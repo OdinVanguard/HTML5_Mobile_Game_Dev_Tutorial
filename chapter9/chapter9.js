6c7ba2598da294c89d2733f2ffee83f82ff9ca01
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2017-08-09 11:52:33 -0700

    "chapter9: setup box2d physics world, added ground"

--- chapter9/chapter9.js
+++ chapter9/chapter9.js
@@ -10,6 +10,9 @@ goog.require('lime.Circle');
 goog.require('lime.Label');
 goog.require('lime.fill.LinearGradient');
 
+goog.require("box2d.Vec2");
+goog.require("box2d.AABB");
+goog.require("box2d.World");
 
 // entrypoint
 chapter9.start = function(){
@@ -37,8 +40,58 @@ chapter9.start = function(){
     
     layer1.appendChild(sky);
     
+    //setup physics
+    //define gravity vector
+    var gravity = new box2d.Vec2(0,100);
+    
+    //AABB means "axis alighend bounding box", limits of physics world
+    var bounds = new box2d.AABB();
+    bounds.minVertex.Set(0,0);
+    bounds.maxVertex.Set(480,320);
+    
+    //create a world
+    var world = new box2d.World(bounds,gravity,false);
+    
+    //create ground////////////////////////////////////////////
+    var floor = new lime.Sprite();
+    floor.setSize(480,40);
+    floor.setFill("#009000");
+    layer1.appendChild(floor);
+    
+    var bodyDef = new box2d.BodyDef();
+    bodyDef.position.Set(240,300); // position of object center
+    
+    //shape definition
+    var shapeDef = new box2d.BoxDef;
+    shapeDef.restitution = 0;
+    shapeDef.density = 0;
+    shapeDef.friction = 1;
+    shapeDef.extents.Set(240,20); //extension in x and y from center
+    
+    bodyDef.AddShape(shapeDef);
+    
+    //create the body in the world
+    var body = world.CreateBody(bodyDef);
+    floor._body = body;
+    
+    //link limejs object to body
+    //source: https://gist.github.com/1758226
+    function updateFromBody(shape){
+        var pos = shape._body.GetCenterPosition();
+        var rot = shape._body.GetRotation();
+        shape.setRotation(-rot/Math.PI*180);
+        shape.setPosition(pos);
+    }
+    
+    dt = 25;
+    lime.scheduleManager.scheduleWithDelay(function(){
+        this.Step(dt/1000,3);
+        updateFromBody(floor);
+    },world,dt)
+    
+    ////////////////////////////////////////////
+    
     director.replaceScene(scene1);
-
 }
 
 

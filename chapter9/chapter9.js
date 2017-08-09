21b833905527d267fadb6b19d32b9334d973ac07
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2017-08-09 13:43:51 -0700

    "chapter9: added falling blocks"

--- chapter9/chapter9.js
+++ chapter9/chapter9.js
@@ -74,7 +74,44 @@ chapter9.start = function(){
     var body = world.CreateBody(bodyDef);
     floor._body = body;
     
-    //link limejs object to body
+    ////////////////////////////////////////////
+    
+    //create blocks////////////////////////////////////////////
+    var num_blocks = 3;
+    var blocks = [];
+    for(i=0;i<num_blocks;i++){
+        //limejs element
+        var width = goog.math.uniformRandom(20,40);
+        var height = goog.math.uniformRandom(20,40);
+        var block = new lime.Sprite();
+        block.setFill("#888878");
+        block.setSize(width,height);
+
+        //body definition
+        bodyDef = new box2d.BodyDef();
+        bodyDef.position.Set(
+            goog.math.uniformRandom(40,440),
+            goog.math.uniformRandom(25,100)); //object center
+
+        //shape definition
+        shapeDef = new box2d.BoxDef;
+        shapeDef.restitution = 0.9;
+        shapeDef.density = 10;
+        shapeDef.friction = 1;
+        shapeDef.extents.Set(width/2,height/2);
+
+        bodyDef.AddShape(shapeDef);
+
+        //create the body in the world 
+        var body = world.CreateBody(bodyDef);
+        block._body = body;
+        blocks.push(block);
+        
+        layer1.appendChild(blocks[i]);
+    }
+    ////////////////////////////////////////////
+    
+    //link limejs objects to body
     //source: https://gist.github.com/1758226
     function updateFromBody(shape){
         var pos = shape._body.GetCenterPosition();
@@ -82,11 +119,13 @@ chapter9.start = function(){
         shape.setRotation(-rot/Math.PI*180);
         shape.setPosition(pos);
     }
-    
     dt = 25;
     lime.scheduleManager.scheduleWithDelay(function(){
         this.Step(dt/1000,3);
         updateFromBody(floor);
+        for(i in blocks){
+            updateFromBody(blocks[i]);
+        }
     },world,dt)
     
     ////////////////////////////////////////////

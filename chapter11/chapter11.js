337e90f1fe4d3a2522ccc809248704158c346f7f
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2017-08-09 16:00:16 -0700

    "chapter11: load map, create click to move player on map"

--- chapter11/chapter11.js
+++ chapter11/chapter11.js
@@ -6,67 +6,51 @@ goog.provide('chapter11');
 goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
-goog.require('lime.Circle');
-goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
+goog.require('lime.parser.TMX');
 goog.require('lime.animation.MoveTo');
 
 
 // entrypoint
 chapter11.start = function(){
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
-
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
-
-
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
-
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
+    var director = new lime.Director(document.body,480,320);
+    director.makeMobileWebAppCapable();
+    director.setDisplayFPS(false);
+    
+    var gameScene = new lime.Scene;
+    layer = new lime.Layer()
+    layer.setRenderer(lime.Renderer.CANVAS);
+    gameScene.appendChild(layer);
+    
+    
+    
+    var tmx = new lime.parser.TMX("assets/lesson11_map.tmx");
+    for(var j = 0;j < tmx.layers.length;j++){
+        for(var i = 0; i < tmx.layers[j].tiles.length; i++){
+            tile = tmx.layers[j].tiles[i];
+            sprite = new lime.Sprite();
+            sprite.setPosition(tile.px,tile.py);
+            sprite.setFill(tile.tile.frame);
+            layer.appendChild(sprite);
+        }
+    }
+    
+    //player
+    var player = new lime.Sprite();
+    player.setPosition(100,100).setFill("assets/player.png");
+    
+    layer.appendChild(player);
+    
+    //move player
+    goog.events.listen(layer,['mousedown','touchstart'],
+        function(e){
+            movement = new lime.animation.MoveTo(e.position.x,e.position.y);
+            console.log("click at"+e.position);
+            movement.setDuration(1);
+            player.runAction(movement);
         });
-
-
-    });
-
-	// set current scene active
-	director.replaceScene(scene);
+    
+    director.replaceScene(gameScene);
 
 }
 
